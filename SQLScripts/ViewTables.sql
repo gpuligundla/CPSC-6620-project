df319298d8c361ecd81e855aff9c6c76cd613f9f
Geethakrishna Puligundla <gpuligundla@clemson.edu>
2023-11-04 19:55:14 -0400

    CPSC6620: script to Display all tables

---
+++ SQLScripts/ViewTables.sql
@@ -0,0 +1,47 @@
+/*
+This script is developed by Geethakrishna Puligundla
+mail: gpuligu@clemson.edu
+
+This script includes populating data into tables which are created in CreateTables.sql
+All copyrights are Reserved.
+*/
+
+-- Database Schema
+use Pizzeria;
+
+-- Display topping Table
+SELECT * FROM topping;
+
+-- Display Discount Table
+SELECT * FROM discount;
+
+-- Display pizzabase Table
+SELECT * FROM pizzabase;
+
+-- Display Customer Table
+SELECT * FROM customer;
+
+-- Display Order History Table
+SELECT * FROM orderhistory;
+
+-- Display Pizza Table
+SELECT * FROM pizza;
+
+-- Display Pizza Topping Table
+SELECT * FROM pizzatopping;
+
+-- Display Order Discount Table
+SELECT * FROM orderdiscount;
+
+-- Display Pizza Discount Table
+SELECT * FROM pizzadiscount;
+
+-- Display DineIN Table
+SELECT * FROM dinein;
+
+-- Display Pickup Table
+SELECT * FROM pickup;
+
+-- Display Home Delivery Table
+SELECT * FROM homedelivery;
+
