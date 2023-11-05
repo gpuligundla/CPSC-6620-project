463e473e3755513cb939b04b513efee7394506ec
Geethakrishna Puligundla <gpuligundla@clemson.edu>
2023-11-04 20:12:25 -0400

    CPSC6620: script to DROP all tables

---
+++ SQLScripts/DropTables.sql
@@ -0,0 +1,47 @@
+/*
+This script is developed by Geethakrishna Puligundla
+mail: gpuligu@clemson.edu
+
+This script includes populating data into tables which are created in CreateTables.sql
+All copyrights are Reserved.
+*/
+
+-- Database Schema
+use Pizzeria;
+
+
+-- Drop Pizza Topping Table
+DROP TABLE pizzatopping;
+
+-- Drop Order Discount Table
+DROP TABLE orderdiscount;
+
+-- Drop Pizza Discount Table
+DROP TABLE pizzadiscount;
+
+-- Drop DineIN Table
+DROP TABLE dinein;
+
+-- Drop Pickup Table
+DROP TABLE pickup;
+
+-- Drop Home Delivery Table
+DROP TABLE homedelivery;
+
+-- Drop Pizza Table
+DROP TABLE pizza;
+
+-- Drop Topping Table
+DROP TABLE topping;
+
+-- Drop Discount Table
+DROP TABLE discount;
+
+-- Drop PizzaBase Table
+DROP TABLE pizzabase;
+
+-- Drop Customer Table
+DROP TABLE customer;
+
+-- Drop Order History Table
+DROP TABLE orderhistory;
